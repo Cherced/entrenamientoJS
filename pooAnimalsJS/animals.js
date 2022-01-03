c8919e10a4d69ab8177823538bae1e08a3c083d2
cherced <Chernandezcediel@gmail.com>
2022-01-03 10:06:21 -0500

    lets start this, i do 2 exercises for classes apply on js

---
+++ pooAnimalsJS/animals.js
@@ -0,0 +1,12 @@
+class Animals {
+    constructor(name, age, species, sex, patology, bio){
+        this.name = name;
+        this.age = age;
+        this.species = species;
+        this.sex = sex;
+        this.patology = patology;
+        this.bio = function (parameters) {
+            
+        }
+    } 
+}
